51a32ddc94b6f5cdcc45d7851021fdb1df2b397e
Bennett Hardwick <me@bennetthardwick.com>
2018-12-02 23:19:45 +1000

    Add day 1

---
+++ solutions/1/solution.js
@@ -0,0 +1,32 @@
+#!/bin/env node
+
+const fs = require('fs');
+const input = fs.readFileSync('./input', { encoding: 'utf8' });
+
+// Use function constructor because gotta go fast
+console.log('Part one!');
+console.log(new Function(`return ${input.split('\n').join(' ')}`)() + '\n');
+
+console.log('Part two!');
+
+let seen = {};
+let total = 0;
+let frequency = '';
+let commands = input.split('\n');
+
+for (let i = 0; i < commands.length; i++) {
+  total = eval(`${total} ${commands[i]}`);
+
+  frequency = String(total);
+
+  if (seen[frequency]) {
+    break;
+  } else {
+    seen[frequency] = true;
+  }
+
+  if (i >= commands.length - 1) i = -1;
+
+}
+
+console.log(frequency);
